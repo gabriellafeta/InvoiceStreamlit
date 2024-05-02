0b2d28e01af46bb3459cade621db42971bd8da1b
gabriellafeta <gabriellafetnascimento@gmail.com>
2024-05-02 19:09:12 -0300

    commit

--- invoiceapp.py
+++ invoiceapp.py
@@ -1,3 +1,9 @@
+import streamlit as st
 import pandas as pd
+from streamlit_gsheets import GSheetsConnection
 
-print("Hello world")
+url = "https://docs.google.com/spreadsheets/d/12Xz2kB_0mos07T-SGy6REs7yDvjxn3Y_5vn8F25T05A/edit#gid=0"
+
+conn = st.experimental_connection("gsheets", type = GSheetsConnection)
+data = conn.read(spreadsheet=url)
+st.dataframe(data)
