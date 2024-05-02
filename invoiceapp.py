7e72d1b2eaef54ddce0c921ee0bc2747d63279b0
gabriellafeta <gabriellafetnascimento@gmail.com>
2024-05-02 19:55:13 -0300

    commit

--- invoiceapp.py
+++ invoiceapp.py
@@ -6,16 +6,6 @@ url = "https://docs.google.com/spreadsheets/d/12Xz2kB_0mos07T-SGy6REs7yDvjxn3Y_5
 
 conn = st.experimental_connection("gsheets", type = GSheetsConnection)
 data = conn.read(spreadsheet=url)
-st.dataframe(data)
 
 df = pd.DataFrame(data)
-
-if 'updated_df' not in st.session_state:
-    st.session_state.updated_df = df.copy()
-
-for i, row in st.session_state.updated_df.iterrows():
-    # The key ensures each checkbox is unique
-    new_value = st.checkbox("Vendido?", value=row['Vendido'], key=i, help="Select if sold")
-    st.session_state.updated_df.at[i, 'Vendido'] = new_value
-
-st.write("Updated DataFrame:", df)
+st.dataframe(df)
